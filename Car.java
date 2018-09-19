bd3b5c5a2171ffd29590e58b4227a8175240ed21
Evan Cann <sEvanCann@yccc.edu>
2018-09-19 18:45:28 -0400

    first commit

---
+++ Car.java
@@ -0,0 +1,46 @@
+
+public class Car {
+	
+
+	public String carModel;
+	public String carColor;
+	public String carTransmission;
+
+	public static void main(String[] args)
+	{
+	
+	
+	
+	}
+	public String getCarModel()
+	{
+	return carModel;	
+	}
+	public void setCarModel(String carModel) 
+	{
+	this.carModel = carModel;
+	}
+
+	
+	public String getCarColor()
+	{
+	return carColor;
+	}
+	public void setCarColor(String carColor) 
+	{
+	this.carColor = carColor;
+	}
+	
+	public String getCarTransmission()
+	{
+	return carTransmission;
+	}
+	public void setCarTransmission(String carTransmission)
+	{
+	this.carTransmission = carTransmission;
+	}
+
+
+	
+
+}
